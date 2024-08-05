68b3d218cc6fbc3cbe4c11a64dff6c6fb79743bd
Shahroz Ahmad <97090106+ishahroz@users.noreply.github.com>
2024-08-05 17:43:03 -0300

    feat: fix fixture file (sqlite3 db data json file to remove contenttypes), added pymysql package

---
+++ backend/__init__.py
@@ -0,0 +1,4 @@
+# To use MySql DB from the package
+import pymysql
+
+pymysql.install_as_MySQLdb()
--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,14 +3,13 @@
 # Kill any servers that may be running in the background
 sudo pkill -f runserver
 
-# Kill frontend servers if you are deploying any frontend
-# sudo pkill -f tailwind
-# sudo pkill -f node
-
 cd /home/ubuntu/otaku-house/
 
 # Activate Virtual Environment
 python3 -m venv venv
+
+sudo chmod -R a+rwx venv
+
 source venv/bin/activate
 
 # Install pip requirements
